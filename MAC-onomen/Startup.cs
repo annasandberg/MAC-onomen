6cc320f01ed3e0c99ad528e5714579b951a47add
mariakallstrom <mariakallstrom@outlook.com>
2017-11-27 11:32:05 +0100

    Add bransh feature/startpage_customer

--- MAC-onomen/Startup.cs
+++ MAC-onomen/Startup.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Net.WebSockets;
-using Microsoft.AspNetCore.Http;
-using System.Threading;
 
 namespace MAC_onomen
 {
@@ -41,7 +34,6 @@ namespace MAC_onomen
             }
 
             app.UseStaticFiles();
-            app.UseWebSockets();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
