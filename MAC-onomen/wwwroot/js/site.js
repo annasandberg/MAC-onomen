1889042700237e4e5c9724b0512925a0e79d1b1a
mariakallstrom <mariakallstrom@outlook.com>
2017-11-27 12:42:42 +0100

    Add SendController - If you click on image object is passed to controller with servicetype and regnumber

--- MAC-onomen/wwwroot/js/site.js
+++ MAC-onomen/wwwroot/js/site.js
@@ -1,1 +1,14 @@
 ﻿// Write your JavaScript code.
+$(".box").click(function () {
+
+    var serviceNumber = $(this).attr("id");
+    var service = $(this).children("h4").html();
+
+    console.log(serviceNumber)
+
+    $("#serviceId").val(serviceNumber);
+   
+    $("#choosenService").html("Du har valt: " + service)
+    
+    
+})
