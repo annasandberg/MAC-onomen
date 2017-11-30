359350691ff68075c0759b39d9f89ec79937c698
Cornelia Karlslund <cornelia.karlslund@gmail.com>
2017-11-30 10:17:54 +0100

    Initial changes.

---
+++ MACmack/Models/ServiceTypeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MACmack.Models
+{
+    public class ServiceTypeViewModel
+    {
+        public string Regnumber { get; set; }
+        public ServiceTypes ServiceTypes { get; set; }
+
+    }
+
+    public enum ServiceTypes
+        {
+        wash,
+        fuel,
+        oil,
+        lamp,
+        tire,
+        washingfluid,
+        wiper,
+        air
+        }
+}
