a7ec28d4a1182936c2898a7d33d1f2c058e8cc76
annasandberg <Anna.Kallman@yh.nackademin.se>
2017-12-01 13:25:23 +0100

    add try catch statemnet when starting server

--- MACserver/Program.cs
+++ MACserver/Program.cs
@@ -19,17 +19,31 @@ namespace WebSocketServer
 
         public static void Main()
         {
+            TcpListener server = StartServer();
 
-            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 8000);
-            server.Start();
-            Console.WriteLine("Server has started on 127.0.0.1:8000.{0}Waiting for a connection...", Environment.NewLine);
+            if (server == null) return;
 
             while (true) // Add exit flag here
             {
                 client = server.AcceptTcpClient();
-                MyClient c = new MyClient(client, ((IPEndPoint)client.Client.RemoteEndPoint).Port.ToString());
-                
-                
+                MyClient c = new MyClient(client, ((IPEndPoint)client.Client.RemoteEndPoint).Port.ToString());   
+            }
+        }
+
+        private static TcpListener StartServer()
+        {
+            try
+            {
+                TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 8000);
+                server.Start();
+                Console.WriteLine("Server has started on 127.0.0.1:8000.{0}Waiting for a connection...", Environment.NewLine);
+                return server;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ett fel uppstod när servern startades.");
+                Console.WriteLine(e.Message);
+                return null;
             }
         }
         
